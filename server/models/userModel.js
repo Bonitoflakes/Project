1e701c538d72298d5bd03c9547d90122fd2cd3fb
Rishab Khivsara <rishabjain3500@gmail.com>
2022-03-11 11:28:04 +0530

    Update: Basic Schema Validation

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,16 +1,19 @@
 import { mongoose } from 'mongoose';
+import pkg from 'validator';
+const { isEmail } = pkg;
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
-        unique: true,
-        maxLength:20
+        required: [true,"Please enter a username"],
+        unique: [true,"Username is already taken"],
+        maxLength:[20,"Username cannot exceed 20 characters"]
     },
     email: {
         type: String,
         required: true,
         unique: true,
+        validate:[isEmail,"Please enter a valid email"]
     },
     password: {
         type: String,
