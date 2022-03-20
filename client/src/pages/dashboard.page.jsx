a7a360c46367bf2c9e50f82abc55d56fa280725f
bonitoflakes <rishabjain3500@gmail.com>
2022-03-20 23:43:10 +0530

    Add : fetch dashboard, render it to the view

--- client/src/pages/dashboard.page.jsx
+++ client/src/pages/dashboard.page.jsx
@@ -1,19 +1,31 @@
-import React, { useEffect } from 'react'
-import { axios } from 'axios';
+import React, { useEffect ,useState } from 'react'
+import  axios  from 'axios';
 
 const Dashboard = () => {
 
+  const [details, setDetails] = useState({})
+
   useEffect( () => {
-    console.log("Welcome to Dashboard");
-    async function fetchData() {
-      const userData = await axios.get("localhost:8000/api/user/dashboard/:userID");
-      console.log(userData);
-    } 
+    console.log("Fetching dashboard details....");
     fetchData();
+
   },[])
 
+  const fetchData = async () => {
+
+    const userData = await axios.get("http://localhost:8000/api/user/dashboard/");
+    setDetails(userData.data.userDetails)
+    console.log(userData.data.userDetails);
+  }
+
+
   return (
-    <div>Dashboard</div>
+    <>
+      <h1>Overall Portfolio: {details.total_holdings_value} </h1>
+      <h2>Total Invested: {(details.total_cost).toFixed(2)} </h2>
+      <h2>24h change: ${details.holdings_change_24h.toFixed(2)}   {details.holdings_change_percentage}%</h2>
+      {(Math.sign(details.profit_loss) > 0) ? <h1>Profit: {details.profit_loss.toFixed(2)}</h1> : <h1>Loss: ${details.profit_loss.toFixed(2)}</h1>}
+    </>
   )
 }
 
