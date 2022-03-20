19192425128a5bc74089cfe947d185e277d67aac
bonitoflakes <rishabjain3500@gmail.com>
2022-03-20 23:06:04 +0530

    Update : changed top level routers

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
-import { router } from "./routes/authRoutes.js";
+import { router as AuthRouter } from "./routes/authRoutes.js";
+import { router as DashboardRouter } from "./routes/dashboardRoutes.js";
 import cors from 'cors';
 
 dotenv.config({ path: "./config/config.env" });
@@ -15,7 +16,8 @@ app.use(cors());
 
 app.use(express.json());
 app.get("/",(req,res)=>res.send("Welcome to HOME"))
-app.use("/api/users",router)
+app.use("/api/users",AuthRouter)
+app.use("/api/user/dashboard",DashboardRouter)
 
 
 // Server init
