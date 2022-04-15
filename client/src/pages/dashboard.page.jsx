2e973b824805052dd3a0ebe0a3198915065d8ad8
bonitoflakes <rishabjain3500@gmail.com>
2022-04-15 18:03:00 +0530

    Update dashboard.page.jsx

--- client/src/pages/dashboard.page.jsx
+++ client/src/pages/dashboard.page.jsx
@@ -22,9 +22,10 @@ const Dashboard = () => {
   return (
     <>
       <h1>Overall Portfolio: {details.total_holdings_value} </h1>
-      <h2>Total Invested: {(details.total_cost).toFixed(2)} </h2>
-      <h2>24h change: ${details.holdings_change_24h.toFixed(2)}   {details.holdings_change_percentage}%</h2>
-      {(Math.sign(details.profit_loss) > 0) ? <h1>Profit: {details.profit_loss.toFixed(2)}</h1> : <h1>Loss: ${details.profit_loss.toFixed(2)}</h1>}
+      <h2>Total Invested: {(details.total_cost)} </h2>
+      <h2>24h change: ${details.holdings_change_24h}   {details.holdings_change_percentage}%</h2>
+      {(Math.sign(details.profit_loss) > 0) ? <h1>Profit: {details.profit_loss}</h1> : <h1>Loss: ${details.profit_loss}</h1>}
+      {2+2}
     </>
   )
 }
